7600425aa2dd94687205053f17deeb96448420f7
Carpe-bleue <numero2342@gmail.com>
2024-09-16 18:05:40 +0200

    création ROS_tutorials

---
+++ ROS_tutorials/tutorial_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/carpe-bleue/Git/2425_Projet2A_AresCFR/ROS_tutorials/tutorial_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/carpe-bleue/ROS/my_workspace/devel;/opt/ros/noetic'.split(';') if '/home/carpe-bleue/ROS/my_workspace/devel;/opt/ros/noetic' != '' else []
