9d99c841e5f3fa87fe99e7ba46172114af7bf640
Sammy Gros <144098583+wamyr@users.noreply.github.com>
2025-03-12 18:15:27 +0000

    Update UART_Node.launch.py

--- 2A/Firmware/ros2_ws/src/serial_package/launch/UART_Node.launch.py
+++ 2A/Firmware/ros2_ws/src/serial_package/launch/UART_Node.launch.py
@@ -4,7 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='UART_Node',
+            package='serial_package',
             executable='UART_Node',
             output='screen'),
     ])
